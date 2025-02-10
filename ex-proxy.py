b5f7d42c551774e8ba0d88006e5dd7507f17c069
huynambka <huynambka@gmail.com>
2025-02-10 05:05:06 -0500

    update 1.21.4 protocol

--- ex-proxy.py
+++ ex-proxy.py
@@ -9,17 +9,98 @@ from twisted.internet import reactor
 from quarry.types.uuid import UUID
 from quarry.net.proxy import DownstreamFactory, Bridge
 import logging
+import math
+import struct
 
 
 class QuietBridge(Bridge):
-    
+
+    entity_id = None
+    prev_pos = None
+    prev_look = None
+
+    def packet_upstream_chat_command(self, buff):
+        buff.save()
+        chat_message = buff.unpack_string()
+        print(f" >> {chat_message}")
+
+        if chat_message.startswith("/port"):
+            _, distance = chat_message.split(" ")
+            flags = 0
+            teleport = 0
+            dismount = 0
+            x, y, z, ground = self.prev_pos
+            yaw, pitch, ground = self.prev_look
+            # see net.minecraft.entity.Entity:getRotationVEctor()
+            f = pitch * 0.017453292
+            g = -yaw * 0.017453292
+            h = math.cos(g)
+            i = math.sin(g)
+            j = math.cos(f)
+            k = math.sin(f)
+            _x = i * j
+            _y = -k
+            _z = h * j
+            x += _x * float(distance)
+            y += _y * float(distance)
+            z += _z * float(distance)
+            buf = struct.pack('>dddffBBB', x, y, z, yaw, pitch, flags, teleport, dismount)
+            self.upstream.send_packet('pos_rot', buf)
+
+        buff.restore()
+        self.upstream.send_packet("chat_command", buff.read())
+
     def packet_unhandled(self, buff, direction, name):
         if direction == "downstream":
+            # print(f"[*][{direction}] {name}")
             self.downstream.send_packet(name, buff.read())
         elif direction == "upstream":
-            print(f"[*][{direction}] {name}")
             self.upstream.send_packet(name, buff.read())
 
+    def packet_downstream_player_position(self, buff):
+        buf = buff.read()
+
+        print(f"[*] player_position: {buf.hex()}")
+
+        self.downstream.send_packet('player_position', buf)
+
+    def packet_downstream_pos(self, buff):
+        buf = buff.read()
+
+        print(f"[*] pos: {buf.hex()}")
+
+        self.downstream.send_packet('pos', buf)
+
+    def packet_downstream_pos_rot(self, buff):
+        buf = buff.read()
+
+        print(f"[*] pos: {buf.hex()}")
+
+        self.downstream.send_packet('pos', buf)
+
+    def packet_upstream_pos(self, buff):
+        buff.save()
+        buf = buff.read()
+        x, y, z, ground = struct.unpack('>dddB', buf)
+        # print(f"[*] player_position {x} / {y} / {z} | {ground}")
+        self.prev_pos = (x, y, z, ground)
+        self.upstream.send_packet('pos', buf)
+
+    def packet_upstream_rot(self, buff):
+        buff.save()
+        yaw, pitch, ground = struct.unpack('>ffB', buff.read())
+        # print(f"[*] player_look {yaw} / {pitch} | {ground}")
+        self.prev_look = (yaw, pitch, ground)
+        buf = struct.pack('>ffB', yaw, pitch, ground)
+        self.upstream.send_packet('rot', buf)
+
+    # def packet_downstream_pos_rot(self, buff):
+    #     buff.save()
+    #     print(buff.read())
+    #     x, y, z, yaw, pitch, flags = struct.unpack('>dddffB', buff.read())
+    #     print(f"[*] player_position_and_look {x} / {y} / {z} | {yaw} / {pitch} | {flags}")
+    #     buf = struct.pack('>dddffB', x, y, z, yaw, pitch, flags)
+    #     self.downstream.send_packet('pos_rot', buf)
 
 
 class QuietDownstreamFactory(DownstreamFactory):
@@ -31,6 +112,7 @@ class QuietDownstreamFactory(DownstreamFactory):
 def main(argv):
     # Parse options
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--listen-host", default="", help="address to listen on")
     parser.add_argument("-p", "--listen-port", default=25565, type=int, help="port to listen on")
@@ -50,4 +132,5 @@ def main(argv):
 
 if __name__ == "__main__":
     import sys
+
     main(sys.argv[1:])
